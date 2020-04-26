ac5fc864efcac42095455dabd34a9242a72190f9
John Doe <johndoe@example.com>
2020-04-26 14:27:08 -0500

    Section 9: added autobind decorator

--- my-code/09_drag-and-drop-project/src/app.ts
+++ my-code/09_drag-and-drop-project/src/app.ts
@@ -1,1 +1,57 @@
-// Code goes here!
+
+// autobind decorator
+function autobind(_target: any, _methodName: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDesriptor: PropertyDescriptor = {
+    configurable: true,
+    
+    get() : string {
+      const boundFn = originalMethod.bind(this); 
+      return boundFn;
+    }
+  };
+  return adjDesriptor;
+}
+
+class ProjectInput {
+  hostElement: HTMLDivElement;
+  templateElement: HTMLTemplateElement;
+  element: HTMLFormElement;
+
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+
+  constructor() {
+    this.hostElement = document.getElementById('app') as HTMLDivElement;
+    this.templateElement = document.getElementById('project-input') as HTMLTemplateElement;
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler);
+  }
+
+  @autobind
+  private submitHandler(ev: Event) {
+    ev.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const projectInput = new ProjectInput();
